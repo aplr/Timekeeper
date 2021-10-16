0c6e6cf2ab557586802466a3bdfe6eb1f47ea74a
Andreas Pfurtscheller <andreas.pfurtscheller@gmail.com>
2021-10-16 03:31:13 +0200

    Fix build_docs.sh

--- build_docs.sh
+++ build_docs.sh
@@ -2,4 +2,6 @@
 
 xcodebuild docbuild -scheme Timekeeper -derivedDataPath ./DerivedData -destination generic/platform=iOS
 
+rm -rf build
+mkdir build
 cp -r DerivedData/Build/Products/Debug-iphoneos/Timekeeper.doccarchive ./build
